7fb77187de7e62d631c07e944129d4da4e823a9b
sourabh kulkarni <skulkar6@stevens.edu>
2017-11-01 02:27:12 -0400

    Final Assignment code

--- DLinkedList.java
+++ DLinkedList.java
@@ -1,83 +1,120 @@
-class DlinkedList{
-    class Node{
+/*
+Author: Sourabh kulkarni
+*/
+
+import java.io.*;
+import java.util.*;
+class DLinkedList{
+    
+    public class Node{      
         int val;
         Node next;
         Node prev;
+        public Node(int v,Node next,Node prev) {// Constructor for Node class
+			val = v; this.next = next; this.prev = prev; 
+		}
 
     }
-public Node(int v,Node next,Node prev){
-    val = v; this.next = next; this.prev = prev; 
-}
+
 
 private Node head;
 private Node tail;
 
-public DlinkedList(){
+public DLinkedList(){       // Constructor for DLinkedList
     head = null;
     tail = null;
 
 }
   // adds a node at the start
-public void addStart(){
+public void addStart(int v){
     Node temp = new Node(v, head, null);
     if(head != null){   // checks if head is present
         head.prev = temp;
 
     }
+    head = temp;         // set head to the newly added node to avoid NullPtrException
 
-    if(tail == null){  // checks if tail is null
-        tail = temp;
+    if(tail == null) 
+    { 
+    tail = temp;
     }
+      
 
 }
 
 // adds node at the end
-public void addEnd(){
-    Node temp = new Node(v, head, null);
-    if( head == null){
+public void addEnd(int v){
+   Node temp = new Node(v, null, tail);
+   if(tail != null) 
+       {
+       tail.next = temp;
+       
+       }
+        tail = temp;    // set tail to the newly added node to avoid NullPtrException
+
+    if(head == null){
         head = temp;
+         
+    }
+    
+     
+
+}
+// removes specified number of nodes from the start
+public void removeStart(int v){
+       
+    for(int i = v; i> 0 ; i--){   // 
+        if(head == null){
+            return;
+        }else{
+
+            head = head.next;       
+            head.prev = null;      // point head to second node and null the first node
+        }
     }
 
-    if(tail != null){
-        tail.next = temp;
+}
+
+// removes specified number of nodes from the end
+public void removeEnd(int v){
+   
+    for(int i = v; i>0 ; i--){
+        if( tail == null){
+            return;
+        }else{
+            tail = tail.prev;
+            tail.next = null;       // point tail to seconLast node and null the last one
+        }
     }
 
 }
 
-public void removeStart(){
 
 
+public int getPos(int pos){
+    Node temp = head;
+    for(int i=0; i< pos; i++){
+        temp = temp.next;
+    }
+    return temp.val;
 }
 
-public void removeEnd(){
-
-
+// gets the size of linked list
+public int size(){
+    int i = 0;
+    Node p;
+    for(p= head; p != null; p =p.next){
+       i++;
+    }
+    return i;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Main function reads the input file
+// Converts all strings to respective data types
 public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(new FileReader("HW4a.txt"));
-        GoodGrowArray a = new GoodGrowArray();
-
+        
+        DLinkedList a = new DLinkedList();
 
         while(sc.hasNext()) {
 		switch(sc.next()) {
@@ -124,26 +161,19 @@ public static void main(String[] args) throws Exception{
             continue;
 
         case "OUTPUT" :
-            a.output();
+            //a.output();
+            
+            for(int i=0;i<a.size();i++){
+                System.out.print(a.getPos(i)+ " ");
+             }
+             System.out.println(" ");
+             
             continue;
             }
            
 
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
